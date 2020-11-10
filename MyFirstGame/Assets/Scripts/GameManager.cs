c795d84677c276667cf07b68ae7e7bf92129b206
Imran <imsifat1@gmail.com>
2020-11-10 17:07:34 +0600

    First Commit

---
+++ MyFirstGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public GameObject levelCompleteUI;
+    public void CompleteLevel()
+    {
+        levelCompleteUI.SetActive(true);
+        Debug.Log("Level Complete");
+    }
+   public void GameOver()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", 2f);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ MyFirstGame/Assets/Scripts/WelcomeScreen.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class WelcomeScreen : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
